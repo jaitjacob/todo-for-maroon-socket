de2f3a370aafe0f975e7279c00b02075d69f2e43
Priyanka Pudi <priyankapudi4u@gmail.com>
2025-06-25 11:43:52 +0530

    added paragraph in app.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 
 function App() {
 
@@ -6,7 +7,7 @@ function App() {
       <h1 class="text-3xl font-bold">
       maroon-socket's Todo App
        </h1>
-       <p>Rechangle box</p>
+      
     </div>
   );
 }
