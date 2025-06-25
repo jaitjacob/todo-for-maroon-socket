cf47026288e1a68fe5975a1aa0d311c7f11a53a4
Compilearner <ayushibaliyan769@gmail.com>
2025-06-25 15:52:37 +0530

    Feat: Build the Signup Component with no form Validation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
+import SignUp from "./Components/SignUp";
 
 function App() {
 
   return (
-    <div>
-      <h1 class="text-3xl font-bold">
-      maroon-socket's Todo App
-       </h1>
-    </div>
+    <>
+       <SignUp/>
+    </>
   );
 }
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,12 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
