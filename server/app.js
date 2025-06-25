fda4f884677d243cc8caa2c4aeaa2c8214568027
jait <jai8.jacob@gmail.com>
2025-06-25 22:17:56 +0530

    initial backend commit (#11)
    * initial commit
    * include package lock file

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const host = 'localhost'
+const port = 3000
+
+app.get('/', (req, res)=>{
+    res.send('todo server is online')
+})
+
+app.listen(port, () => {
+    console.log(`server listening on ${host}:${port}`)
+})
